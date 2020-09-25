8bc6a03b90477ce796eeae231a322438fe237488
Republik631 <47197794+Republik631@users.noreply.github.com>
2020-09-25 11:44:39 -0500

    Employee Page

--- layout/scripts/googleform-suggest.js
+++ layout/scripts/googleform-suggest.js
@@ -9,7 +9,7 @@ const scriptURL = 'https://script.google.com/macros/s/AKfycbwpFlg4tQeHE7pPUOD7d6
       .then((response) => {
         console.log('Success!', response);
         form.style.display = "none";
-        document.getElementById("submissionMessage").innerHTML = "</br>Got it. Thanks!</br></br><ul class='nospace group element btmspace-80'><li class='one_half first centered small_screen_stretch'><article><a href='https://uah.link'><div class='txtwrap alt-full-border'>Return to Main Page</div></a></article></li></ul>";
+        document.getElementById("submissionMessage").innerHTML = "</br>Got it. Thanks!</br></br><ul class='nospace group element btmspace-80'><li class='one_half first centered small_screen_stretch'><article><a href='.'><div class='txtwrap yellow-full-border'>Return to Main Page</div></a></article></li></ul>";
         document.getElementById("formResponse").style.display = "block";
         document.getElementById("submit").value = "Send";
         document.getElementById("submit").disabled = false;
